bb6d148623fa17e13269e51b5cb116d32ed20c6e
kgrindon <kar.g@outlook.com>
2018-10-31 20:50:35 +0000

    Added Meraki device pull

--- Get-MerakiDashboardInfo.ps1
+++ Get-MerakiDashboardInfo.ps1
@@ -15,7 +15,7 @@ cls
 
 try {
     $orgID = Invoke-RestMethod -Method GET -Uri "$merakiUrlPrefix/organizations" -Headers $headers
-    Write-Host "Meraki Org ID is $($orgID.id)"
+    Write-Host "Meraki Org ID is: $($orgID.id)"
 }
 
 catch {
@@ -24,9 +24,26 @@ catch {
 
 try {
     $networkID = Invoke-RestMethod -Method GET -Uri "$merakiUrlPrefix/organizations/$($orgID.id)/networks" -Headers $headers
-    Write-Host "Meraki network ID<s> are $($networkID.id)"
+    Write-Host "Meraki network ID<s> are: $($networkID.id)"
 }
 
 catch {
     Write-Host "Unable to get network ID<s>"
+}
+
+try {
+    
+    $devices = @()
+
+    foreach ($network in $networkID.id) {
+
+        $devices += Invoke-RestMethod -Method GET -Uri "$merakiUrlPrefix/networks/$network/devices" -Headers $headers
+    
+    }
+    
+    Write-Host "Meraki device information found."
+}
+
+catch {
+    Write-Host "Unable to get Meraki device information."
 }
